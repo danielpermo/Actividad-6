d282c611cd672fa8df227324bdd5dcf2ffbb7471
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-02-17 10:11:54 +0100

    Creación interfaz user para el servicio users. Creación componente user para las cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewuserComponent } from './components/newuser/newuser.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NewuserComponent
+    NewuserComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
