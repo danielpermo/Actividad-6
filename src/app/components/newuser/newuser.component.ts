97596aa6d2c4dc09247fdfa1863d00628afa593a
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-02-19 15:02:26 +0100

    Creación del formulario y validación de datos

--- src/app/components/newuser/newuser.component.ts
+++ src/app/components/newuser/newuser.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-newuser',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./newuser.component.css']
 })
 export class NewuserComponent {
+
+  myForm: FormGroup
+
+  constructor() {
+    this.myForm = new FormGroup({
+      name: new FormControl("", [
+        Validators.required
+      ]),
+      surname: new FormControl("", [
+        Validators.required
+      ]),
+      email: new FormControl("", [
+        Validators.required
+      ]),
+      image: new FormControl("", [
+        Validators.required
+      ])
+    }, []);
+  }
+
+  saveDataForm() {
+    console.log(this.myForm.value);
+  }
 
 }
