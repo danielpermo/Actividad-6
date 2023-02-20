937eac288e1c9760ca2257cb0aaf9a427a9fc0ce
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-02-20 10:52:43 +0100

    Definición de los elementos de la interfaz y tipo User en el service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+ 
+  private usersArr: User[] = []
+
+  constructor() {
+  }
+  
+  getUsers() {
+    console.log(this.usersArr); //Hay que quitar este console.log
+    return this.usersArr;
+   }
+
 }
