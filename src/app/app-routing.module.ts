cada1f4bfac2b01dd288ceefa568fdfb568ab2f2
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-02-20 08:52:12 +0100

    Creación de componentes y rutas userView y updateUser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NewuserComponent } from './components/newuser/newuser.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
 
 const routes: Routes = [
   {path: "", pathMatch: 'full', redirectTo: '/home'},
   {path: "home", component: HomeComponent},
-  {path: "newuser", component: NewuserComponent}
+  {path: "newuser", component: NewuserComponent},
+  {path: "user/:userId", component: UserViewComponent},
+  {path: "updateuser/:userId", component: UpdateUserComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewuserComponent } from './components/newuser/newuser.component';
 import { UserComponent } from './components/user/user.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NewuserComponent,
-    UserComponent
+    UserComponent,
+    UserViewComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
