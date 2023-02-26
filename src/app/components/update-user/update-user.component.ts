9671bbf3306ba6f037270837e9b510970c6ebf49
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-02-26 13:33:11 +0100

    Recogida de valores del updateuser

--- src/app/components/update-user/update-user.component.ts
+++ src/app/components/update-user/update-user.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-update-user',
   templateUrl: './update-user.component.html',
   styleUrls: ['./update-user.component.css']
 })
-export class UpdateUserComponent {
+export class UpdateUserComponent implements OnInit{
 
   updateForm: FormGroup
-
-  constructor() {
+  pId: string = "";
+  myUser: any = {};
+  
+  constructor( private activatedRoute: ActivatedRoute, private usersService: UsersService ) {
     this.updateForm = new FormGroup({
       name: new FormControl("", [
         Validators.required
@@ -26,9 +31,17 @@ export class UpdateUserComponent {
       ])
     }, []);
   }
+  
+  async ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.pId=params['userId']
+    })
+    this.myUser = await this.usersService.getUserbyId(this.pId);
+  }
 
+  
+  
   saveDataUpdateForm() {
     console.log(this.updateForm.value);
   }
-
 }
