a1a949c14c7c3ea5422ae2d1efc9a5f044868d1c
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-03-05 03:00:44 +0100

    Modal para eliminar usuario en user-card

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user.interface';
 import { UsersService } from 'src/app/services/users.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-user-card',
@@ -20,11 +21,27 @@ export class UserCardComponent implements OnInit {
   }
 
   async deleteUser(pId: string) {
-    let response = await this.usersService.deleteUser(pId);
-    console.log(response);
-    // alert('Usuario borrado correctamente');
-  }
-  confirm() {
-    alert('Usuario borrado correctamente');
+    let response: any = await this.usersService.getUserbyId(pId);
+    
+    Swal.fire({
+      title: `¿Desea borrar el usuario ${response.first_name} ?`,
+      text: "¿Si lo haces, no podrás volver atrás!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#082147',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, borrar'
+    }).then(async(result) => {
+      if (result.isConfirmed) {
+        response = await this.usersService.deleteUser(response._id);
+        console.log(response);
+        
+        Swal.fire(
+          '¡Eliminado!',
+          `El usuario ${response.first_name} ha sido borrado correctamente`,
+          'success'
+        )
+      }
+    })
   }
 }
